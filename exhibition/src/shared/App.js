334ff1d9ad63cc221ad41f7dd4ff7f2c8fd68989
aronglee <arong16@naver.com>
2021-10-11 17:24:14 +0900

    First Upload

---
+++ exhibition/src/shared/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <React.Fragment></React.Fragment>
+    </div>
+  );
+}
+
+export default App;
